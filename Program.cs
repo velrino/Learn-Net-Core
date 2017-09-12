8798263d02e9d029ff15227f03c57b6923cd3212
Velrino <hi@velrino.com>
2017-09-12 02:39:28 +0000

    The DateTime Type

--- Program.cs
+++ Program.cs
@@ -6,33 +6,21 @@ namespace hwapp
     {
         static void Main()
         {
-            string one = "abc";
-            string two = "123";
-            string combined = one + two; // "abc123"
+            var currentTime = DateTime.Now; // current time
+            var today = DateTime.Today; // current date - time is midnight
+            var someDate = new DateTime(2016,7,1); // 1 July 2016, midnight
+            var someMoment = new DateTime(2016,7,1,8,0,0); // 1 July 2016, 08:00.00
+            var tomorrow = DateTime.Today.AddDays(1);
+            var yesterday = DateTime.Today.AddDays(-1);
+            var someDay = DateTime.Parse("7/1/2016");
 
-            Console.WriteLine(combined);
-
-            string original = "Test string";
-            string capital = original.ToUpper(); // TEST STRING
-            string lower = original.ToLower(); // test string
-            string lower2 = "Another Test".ToLower(); // another test
-
-            Console.WriteLine(original);
-            Console.WriteLine(capital);
-            Console.WriteLine(lower);
-            Console.WriteLine(lower2);
-        
-            string input = " Steve "; // has a space at the start and end.
-            string clean1 = input.TrimStart(); // "Steve "
-            string clean2 = input.TrimEnd(); // " Steve"
-            string clean3 = input.Trim(); // "Steve"
-            string shortversion = input.Trim().Substring(0,3); // "Ste"
-
-            Console.WriteLine($"*{input}*");
-            Console.WriteLine($"*{clean1}*");
-            Console.WriteLine($"*{clean2}*");
-            Console.WriteLine($"*{clean3}*");
-            Console.WriteLine($"*{shortversion}*");
+            Console.WriteLine($"{nameof(currentTime)}: {currentTime}");
+            Console.WriteLine($"{nameof(today)}: {today}");
+            Console.WriteLine($"{nameof(someDate)}: {someDate}");
+            Console.WriteLine($"{nameof(someMoment)}: {someMoment}");
+            Console.WriteLine($"{nameof(tomorrow)}: {tomorrow}");
+            Console.WriteLine($"{nameof(yesterday)}: {yesterday}");
+            Console.WriteLine($"{nameof(someDay)}: {someDay}");
         
         }
     }
