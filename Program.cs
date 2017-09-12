08e4b8acbe1c5365adfc35a8aea8f7b5f6fb9593
Velrino <hi@velrino.com>
2017-09-12 02:40:16 +0000

    Getting to Parts of Dates and Times

--- Program.cs
+++ Program.cs
@@ -6,21 +6,21 @@ namespace hwapp
     {
         static void Main()
         {
-            var currentTime = DateTime.Now; // current time
-            var today = DateTime.Today; // current date - time is midnight
-            var someDate = new DateTime(2016,7,1); // 1 July 2016, midnight
-            var someMoment = new DateTime(2016,7,1,8,0,0); // 1 July 2016, 08:00.00
-            var tomorrow = DateTime.Today.AddDays(1);
-            var yesterday = DateTime.Today.AddDays(-1);
-            var someDay = DateTime.Parse("7/1/2016");
+            var someTime = new DateTime(2016,7,1,11,10,9); // 1 July 2016 11:10:09 AM
+            int year = someTime.Year; // 2016
+            int month = someTime.Month; // 7
+            int day = someTime.Day; // 1
+            int hour = someTime.Hour; // 11
+            int minute = someTime.Minute; // 10
+            int second = someTime.Second; // 9
 
-            Console.WriteLine($"{nameof(currentTime)}: {currentTime}");
-            Console.WriteLine($"{nameof(today)}: {today}");
-            Console.WriteLine($"{nameof(someDate)}: {someDate}");
-            Console.WriteLine($"{nameof(someMoment)}: {someMoment}");
-            Console.WriteLine($"{nameof(tomorrow)}: {tomorrow}");
-            Console.WriteLine($"{nameof(yesterday)}: {yesterday}");
-            Console.WriteLine($"{nameof(someDay)}: {someDay}");
+            Console.WriteLine($"{nameof(someTime)}: {someTime}");
+            Console.WriteLine($"{nameof(year)}: {year}");
+            Console.WriteLine($"{nameof(month)}: {month}");
+            Console.WriteLine($"{nameof(day)}: {day}");
+            Console.WriteLine($"{nameof(hour)}: {hour}");
+            Console.WriteLine($"{nameof(minute)}: {minute}");
+            Console.WriteLine($"{nameof(second)}: {second}");
         
         }
     }
