79b169427a17351d0bb37e53545eb61a0cd6f3c3
Velrino <hi@velrino.com>
2017-09-12 02:42:31 +0000

    Calculating Durations Between DateTimes

--- Program.cs
+++ Program.cs
@@ -6,22 +6,9 @@ namespace hwapp
     {
         static void Main()
         {
-            var someTime = new DateTime(2016,7,1,11,10,9); // 1 July 2016 11:10:09 AM
-            int year = someTime.Year; // 2016
-            int month = someTime.Month; // 7
-            int day = someTime.Day; // 1
-            int hour = someTime.Hour; // 11
-            int minute = someTime.Minute; // 10
-            int second = someTime.Second; // 9
-
-            Console.WriteLine($"{nameof(someTime)}: {someTime}");
-            Console.WriteLine($"{nameof(year)}: {year}");
-            Console.WriteLine($"{nameof(month)}: {month}");
-            Console.WriteLine($"{nameof(day)}: {day}");
-            Console.WriteLine($"{nameof(hour)}: {hour}");
-            Console.WriteLine($"{nameof(minute)}: {minute}");
-            Console.WriteLine($"{nameof(second)}: {second}");
-        
+            DateTime nextYear = new DateTime(DateTime.Today.Year+1, 1, 1);
+            TimeSpan duration = nextYear - DateTime.Today;
+            Console.WriteLine($"There are {duration.TotalDays} days left in the year");
         }
     }
 }
